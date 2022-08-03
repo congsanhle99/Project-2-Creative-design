1fff7caa5e4df42ea373415acf6fbe770d5301d9
CongSanhLe <congsanhle1999@gmail.com>
2022-08-03 23:10:11 +0700

    Complete Project-2-Creative-design

---
+++ assets/script/main.js
@@ -0,0 +1,7 @@
+const a = document.querySelectorAll(".story-btn").forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("change");
+    item.nextElementSibling.classList.toggle("change");
+  });
+});
+console.log(a);
